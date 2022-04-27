9ccd4cba5273305bbaeca0aaac3d6aff2daa4527
shgur7236 <shgurtns7236@naver.com>
2022-04-27 10:46:04 +0900

    정적내부클래스

--- java/다양한 기능들/InnerClass/InnerTest.java
+++ java/다양한 기능들/InnerClass/InnerTest.java
@@ -17,22 +17,50 @@ class OutClass {
         void inTest() {
             System.out.println("OutClass num = " + num + "(외부 클래스의 인스턴스 변수)");
             System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
-            System.out.println("InClass inNum = " + inNum + "(내부 클래스의 인스턴스 변수)");
+            System.out.println("InClass sNum = " + inNum + "(내부 클래스의 스태틱 변수)");
         }
+
+        //static void sTest(){
+
+        //}
     }
 
     public void usingClass() {
         inClass.inTest(); //내부 클래스 변수를 사용하여 메서드 호출하기기
+    }
+
+    static class InStaticClass {
+
+        int inNum = 100;
+        static int sInNum = 200;
+
+        void inTest() { //정적 클래스의 일반 메서드
+            System.out.println("OutClass num = " + inNum + "(외부 클래스의 인스턴스 변수)");
+            System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
+            System.out.println("InClass inNum = " + sInNum + "(내부 클래스의 인스턴스 변수)");
+        }
+        static void sTest() { //정적 클래스의 일반 메서드
+            //System.out.println("OutClass num = " + inNum + "(외부 클래스의 인스턴스 변수)");
+            System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
+            System.out.println("InClass inNum = " + sInNum + "(내부 클래스의 인스턴스 변수)");
+        }
     }
 }
 
     public class InnerTest {
         public static void main(String[] args) {
 
-            OutClass outClass = new OutClass();
+            /*OutClass outClass = new OutClass();
             outClass.usingClass();
 
             OutClass.Inclass inner = outClass.new Inclass();
-            inner.inTest();
+            inner.inTest();*/
+
+            OutClass.InStaticClass sInClass = new OutClass.InStaticClass();
+            sInClass.inTest();
+
+            System.out.println();
+
+            OutClass.InStaticClass.sTest();
         }
     }
