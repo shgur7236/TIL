bfc3c882faf96c8e1d98beab1f4c59e771f0ee8a
shgur7236 <shgurtns7236@naver.com>
2022-04-28 17:11:52 +0900

    reduce()메서드

---
+++ java/다양한 기능들/reduce메서드/ReduceTest.java
@@ -0,0 +1,28 @@
+package reduce메서드;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+
+class CompareString implements BinaryOperator<String>{
+
+    @Override
+    public String apply(String s1, String s2) {
+        if (s1.getBytes().length >= s2.getBytes().length) return s1;
+        else return s2;
+    }
+}
+public class ReduceTest {
+
+    public static void main(String[] args){
+
+        String[] greetings = {"안녕하세여*", "hello", "good morning", "반갑습니다"};
+        System.out.println(Arrays.stream(greetings).reduce("",(s1,s2) -> {
+            if (s1.getBytes().length >= s2.getBytes().length) return s1;
+            else return s2;}
+        ));
+
+        String str = Arrays.stream(greetings).reduce(new CompareString()).get();
+        System.out.println(str);
+    }
+}
