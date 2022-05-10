c08c4ae3467d3fb84cd8f04fca7fa456b372a9f3
shgur7236 <shgurtns7236@naver.com>
2022-05-10 22:56:58 +0900

    validation 사용

--- Spring/validation/src/main/java/com/example/validation/controller/ApiController.java
+++ Spring/validation/src/main/java/com/example/validation/controller/ApiController.java
@@ -1,4 +1,52 @@
 package com.example.validation.controller;
 
+import com.example.validation.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
 public class ApiController {
+
+    @PostMapping("/user")
+    public ResponseEntity user(@Valid @RequestBody User user, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            StringBuilder sb = new StringBuilder();
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError field = (FieldError) objectError;
+                String message = objectError.getDefaultMessage();
+
+                System.out.println("field : " + field.getField());
+                System.out.println(message);
+
+                sb.append("field : " +field.getField());
+                sb.append("message : " +message);
+
+
+            });
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sb.toString());
+        }
+
+        System.out.println(user);
+
+        if(user.getAge() >= 90) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(user);
+        }
+
+        //logic
+
+        return ResponseEntity.ok(user);
+    }
+
+
 }
---
+++ Spring/validation/src/main/java/com/example/validation/dto/User.java
@@ -0,0 +1,51 @@
+package com.example.validation.dto;
+
+import javax.validation.constraints.*;
+
+public class User {
+
+    @NotBlank
+    private String name;
+
+    @Max(value = 90)
+    private int age;
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")
+    private String phoneNumber;
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
