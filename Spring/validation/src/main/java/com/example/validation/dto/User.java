d14b8bc9ed2bc7232b21a87def14a758608fd212
shgur7236 <shgurtns7236@naver.com>
2022-05-11 17:07:18 +0900

    custom validation 사용

--- Spring/validation/src/main/java/com/example/validation/dto/User.java
+++ Spring/validation/src/main/java/com/example/validation/dto/User.java
@@ -1,43 +1,25 @@
 package com.example.validation.dto;
 
+import com.example.validation.annotation.YearMonth;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.Valid;
 import javax.validation.constraints.*;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class User {
 
     @NotBlank
     private String name;
 
+    @Valid
+    private List<Car> cars;
+
     @Max(value = 90)
     private int age;
-
-    @Email
-    private String email;
-
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")
-    private String phoneNumber;
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 
     public String getName() {
         return name;
@@ -47,5 +29,19 @@ public class User {
         this.name = name;
     }
 
+    public List<Car> getCars() {
+        return cars;
+    }
 
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
---
+++ Spring/validation/src/main/java/com/example/validation/validator/YearMonthValidator.java
@@ -0,0 +1,30 @@
+package com.example.validation.validator;
+
+import com.example.validation.annotation.YearMonth;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class YearMonthValidator implements ConstraintValidator<YearMonth, String> {
+
+    private String pattern;
+
+    @Override
+    public void initialize(YearMonth constraintAnnotation) {
+        this.pattern = constraintAnnotation.pattern();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        //yyyyMM01
+        try{
+            LocalDate localDate = LocalDate.parse(value+"01", DateTimeFormatter.ofPattern(this.pattern));
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+
+    }
+}
