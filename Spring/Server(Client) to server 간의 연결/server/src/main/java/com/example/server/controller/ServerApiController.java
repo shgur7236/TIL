f9055ab2e76a05f2bd11e9c974d3ec6fbf0999fc
shgur7236 <shgurtns7236@naver.com>
2022-05-15 17:34:55 +0900

    naver 지역 검색 api 연동

--- Spring/Server(Client) to server 간의 연결/server/src/main/java/com/example/server/controller/ServerApiController.java
+++ Spring/Server(Client) to server 간의 연결/server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -5,13 +5,59 @@ import com.example.server.dto.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
 
+    //https://openapi.naver.com/v1/search/local.json
+    // ?query=%EB%A6%AC%EB%B7%B0
+    // &display=10
+    // &start=1
+    // &sort=sim
+    @GetMapping("/naver")
+    public String naver(){
+
+        String query = "리뷰";
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query" , "쇼핑")
+                .queryParam("display", 10)
+                .queryParam("sort", "random")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+
+        log.info("uri : {}", uri);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id","SRU94GaYHVtUPdTWOdte")
+                .header("X-Naver-Client-Secret", "xMK5KcPYWI")
+                .build();
+
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
+
+
+
+    }
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age){
         User user = new User();
